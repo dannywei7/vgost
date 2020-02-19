c8082274727164ddbee957772a10661287bbf790
dannywei7 <dannywei7@gmail.com>
2020-02-19 21:51:48 +0800

    Update gostConfig1.sh

--- gostConfig1.sh
+++ gostConfig1.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+mkdir ./log
 
-nohup ./gost -L wss://neo:5b73add1@0.0.0.0:20?compression=true&
+nohup ./gost -L wss://neo:5b73add1@0.0.0.0:20?compression=true >/dev/null 2>./log/gost20.log& 
 
-nohup ./gost -L wss://neo:5b73add1@0.0.0.0:1433?compression=true&
+nohup ./gost -L wss://neo:5b73add1@0.0.0.0:1433?compression=true >/dev/null 2>./log/gost1433.log&
 
-nohup ./gost -L wss://neo:5b73add1@0.0.0.0:3306?compression=true&
+nohup ./gost -L wss://neo:5b73add1@0.0.0.0:3306?compression=true >/dev/null 2>./log/gost3306.log&
 
-nohup ./gost -L socks5://:3389&
+nohup ./gost -L socks5://:3389 >/dev/null 2>./log/gost3389.log&
 
-nohup ./gost -L https://:8080&
+nohup ./gost -L https://:8080 >/dev/null 2>./log/gost8080.log&
