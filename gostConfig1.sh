e2ef1f0099fb1437d17105ebefe374236cdc6c78
dannywei7 <dannywei7@gmail.com>
2020-02-19 11:57:52 +0800

    Add files via upload

---
+++ gostConfig1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+nohup ./gost -L wss://neo:5b73add1@0.0.0.0:20?compression=true&
+
+nohup ./gost -L wss://neo:5b73add1@0.0.0.0:1433?compression=true&
+
+nohup ./gost -L wss://neo:5b73add1@0.0.0.0:3306?compression=true&
+
+nohup ./gost -L socks5://:3389&
+
+nohup ./gost -L http://:8080&
