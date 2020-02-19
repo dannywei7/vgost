b97e37e85381d91461ae3419d25ac70962ba3984
dannywei7 <dannywei7@gmail.com>
2020-02-19 22:56:01 +0800

    Update gostConfig1.sh

--- gostConfig1.sh
+++ gostConfig1.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 mkdir ./log
 
-nohup ./gost -L wss://neo:5b73add1@0.0.0.0:20?compression=true >/dev/null 2>&1& 
+nohup ./gost -L wss://neo:5b73add1@0.0.0.0:20?compression=true >/dev/null 2>./log/gost20.log& 
 
-nohup ./gost -L wss://neo:5b73add1@0.0.0.0:1433?compression=true >/dev/null 2>&1&
+nohup ./gost -L wss://neo:5b73add1@0.0.0.0:1433?compression=true >/dev/null 2>./log/gost1433.log&
 
-nohup ./gost -L wss://neo:5b73add1@0.0.0.0:3306?compression=true >/dev/null 2>&1&
+nohup ./gost -L wss://neo:5b73add1@0.0.0.0:3306?compression=true >/dev/null 2>./log/gost3306.log&
 
-nohup ./gost -L socks5://:3389 >/dev/null 2>&1&
-
-nohup ./gost -L https://:8080 >/dev/null 2>&1&
+##ps aux|grep gost|grep -v grep|cut -c 9-15|xargs kill -15
+##ps -ef |grep gost
