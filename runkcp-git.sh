66763c2db30475c25e09b425b8c644e7f2395ea9
dannywei7 <dannywei7@gmail.com>
2020-06-24 13:13:36 +0800

    Update runkcp-git.sh

--- runkcp-git.sh
+++ runkcp-git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-nohup ./g529 -L kcp://id:pass@:8801 >/dev/null 2>./log/g8801.log& 
-nohup ./g529 -L kcp://id:pass@:8802 >/dev/null 2>./log/g8802.log& 
-nohup ./g529 -L kcp://id:pass@:8803 >/dev/null 2>./log/g8803.log& 
-nohup ./g529 -L kcp://id:pass@:8804 >/dev/null 2>./log/g8804.log& 
-nohup ./g529 -L kcp://id:pass@:8805 >/dev/null 2>./log/g8805.log& 
+nohup ./g611 -L kcp://id:pass@:8801 >/dev/null 2>./log/g8801.log& 
+nohup ./g611 -L kcp://id:pass@:8802 >/dev/null 2>./log/g8802.log& 
+nohup ./g611 -L kcp://id:pass@:8803 >/dev/null 2>./log/g8803.log& 
+nohup ./g611 -L kcp://id:pass@:8804 >/dev/null 2>./log/g8804.log& 
+nohup ./g611 -L kcp://id:pass@:8805 >/dev/null 2>./log/g8805.log& 
